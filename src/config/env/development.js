0101ca2d92be13eb7d2f4f6e4fda0c2d7d991790
AkshayDigheGithub <digheakshay2014@gmail.com>
2019-07-23 16:36:06 +0530

    user email added for nodemailer

--- src/config/env/development.js
+++ src/config/env/development.js
@@ -1,4 +1,8 @@
 export const devConfig = {
     secret: 'IM_SECRET',
-    profile_pic: "https://randomuser.me/api/portraits/med/men/8.jpg"
+    profile_pic: "https://randomuser.me/api/portraits/med/men/8.jpg",
+    email: {
+        user: "user",
+        password: "pass"
+    }
 }
